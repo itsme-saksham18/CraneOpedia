afa11023b9640b0214ec77636003070f93b1738e
Saksham Maheshwari <sakshammaheshwari18@gmail.com>
2025-12-05 17:30:42 +0530

    Database connectivity

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
